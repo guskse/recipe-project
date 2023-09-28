f7f2e4be38ada353a5d8ab57b773d5187b782306
gustavo <guskse@gmail.com>
2023-09-28 15:03:07 -0400

    added styles, routes and more

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import './App.css'
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+//page components
+import Home from "./pages/home/Home";
+import Search from "./pages/search/Search";
+import Recipe from "./pages/recipe/Recipe";
+import Create from "./pages/create/Create";
+
+//other components
+import Navbar from "./components/Navbar"
+
+//styles
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>My Recipee Project</h1>
+      <BrowserRouter>
+
+        <Navbar />
+
+        <Switch>
+          <Route exact path="/"> <Home /> </Route>
+          <Route exact path="/create"> <Create /> </Route>
+          <Route exact path="/search"> <Search /> </Route>
+          <Route exact path="/recipes/:id"> <Recipe /> </Route>
+        </Switch>
+
+
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
